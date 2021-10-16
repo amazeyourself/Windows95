74e4aa3de1de17e9bb0b16ce63455f230b64a7d4
CallMeKitsu <kitsu.tnh.contact@gmail.com>
2021-10-16 13:19:07 +0200

    object oriented

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import pygame
 
+
+class Game:
+    def __init__(self, screen):
+        self.screen = screen
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+    def handling_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def update(self):
+        pass
+
+    def display(self):
+        self.screen.fill('#018382')
+        pygame.display.flip()
+
+    def run(self):
+        while self.running:
+            self.handling_events()
+            self.update()
+            self.display()
+            self.clock.tick(60)
+
+
 pygame.init()
 screen = pygame.display.set_mode((960, 720))
-clock = pygame.time.Clock()
-run = True
-
-err_img = pygame.image.load("./assets/err.png").convert()
-
-x = 0
-y = 0
-
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_LEFT]:
-        x -= 1
-    if pressed[pygame.K_RIGHT]:
-        x += 1
-    if pressed[pygame.K_UP]:
-        y -= 1
-    if pressed[pygame.K_DOWN]:
-        y += 1
-
-    screen.fill("#018382")
-    screen.blit(err_img, (x, y))
-    clock.tick(60)
-    pygame.display.flip()
+pygame.display.set_caption('Windows95 - CallMeKitsu.')
+Game(screen).run()
 
 pygame.quit()
